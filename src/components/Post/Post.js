53bba6a5075d0775f9e7afd33aa1be881ca0da05
Kamigami55 <eason@easonchang.com>
2021-02-12 22:20:37 +0800

    Hide Author section in post

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -1,7 +1,7 @@
 // @flow strict
 import React from 'react';
 import { Link } from 'gatsby';
-import Author from './Author';
+// import Author from './Author';
 import Comments from './Comments';
 import Content from './Content';
 import Meta from './Meta';
@@ -29,7 +29,7 @@ const Post = ({ post }: Props) => {
       <div className={styles['post__footer']}>
         <Meta date={date} />
         {tags && tagSlugs && <Tags tags={tags} tagSlugs={tagSlugs} />}
-        <Author />
+        {/* <Author /> */}
       </div>
 
       <div className={styles['post__comments']}>
