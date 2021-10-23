94d3e38474541670f6802007ff14a193bb875227
Kamigami55 <eason@easonchang.com>
2021-10-24 00:28:02 +0800

    Add likecoin to post page

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -1,6 +1,9 @@
 // @flow strict
 import React from 'react';
 import { Link } from 'gatsby';
+
+import LikeCoin from 'react-likecoin';
+
 // import Author from './Author';
 import Comments from './Comments';
 import Content from './Content';
@@ -8,19 +11,23 @@ import Meta from './Meta';
 import Tags from './Tags';
 import styles from './Post.module.scss';
 import type { Node } from '../../types';
+import { useSiteMetadata } from '../../hooks';
 
 type Props = {
-  post: Node
+  post: Node,
 };
 
 const Post = ({ post }: Props) => {
   const { html } = post;
   const { tagSlugs, slug } = post.fields;
   const { tags, title, date } = post.frontmatter;
+  const { url } = useSiteMetadata();
 
   return (
     <div className={styles['post']}>
-      <Link className={styles['post__home-button']} to="/">All Articles</Link>
+      <Link className={styles['post__home-button']} to="/">
+        All Articles
+      </Link>
 
       <div className={styles['post__content']}>
         <Content body={html} title={title} />
@@ -28,6 +35,11 @@ const Post = ({ post }: Props) => {
 
       <div className={styles['post__footer']}>
         <Meta date={date} />
+        <LikeCoin
+          userId="easonchang"
+          referrer={url + slug}
+          style={{ width: '1px', minWidth: '100%', minHeight: 180 }}
+        />
         {tags && tagSlugs && <Tags tags={tags} tagSlugs={tagSlugs} />}
         {/* <Author /> */}
       </div>
